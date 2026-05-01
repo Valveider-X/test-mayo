81a91f4be2b2b2a439f0e2f1d81215f5696df2bd
Xavi <xavier.valverde926@comunidadunir.net>
2026-05-01 13:31:44 +0200

    [ADD] Arquitectura Nube, Programacion Moviles, psp, psp-kahoot, sostenibilidad

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const appRoot = document.querySelector("#app");
 
-const subjectFiles = ["Acceso a Datos.json", "Desarrollo de Interfaces.json", "Digitalizacion.json", "IPE.json" ];
+const subjectFiles = ["Acceso a Datos.json", "PSP", "Arquitectura Nube.json", "Programación Móviles.json","Desarrollo de Interfaces.json", "SGE.json", "psp-kahoot.json", "Digitalizacion.json", "IPE.json", "Sostenibilidad.json" ];
 const countOptions = [10, 20, 50, "all", "custom"];
 
 const initialState = {
